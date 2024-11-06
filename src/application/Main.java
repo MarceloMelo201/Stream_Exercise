858c9dc56c1118c98753531a944812cb5cd50e6b
Marcelo Melo <marcelovmelo201@gmail.com>
2024-11-06 16:32:17 -0300

    commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,8 +8,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
